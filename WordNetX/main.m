68e2c77ae7dec55b065bad53935d07e2470dec77
Jan <github@geheimwerk.de>
2015-02-09 17:12:56 +0100

    Rebuilding JAM target as native target.

---
+++ WordNetX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WordNetX
+//
+//  Created by Jan on 09.02.15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
